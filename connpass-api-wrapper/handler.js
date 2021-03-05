643124cd909e9fdc1bf0e03489f8db3ad49eaef0
shinbunbun <takamuneyuto@gmail.com>
2021-03-05 23:25:49 +0900

    first commit

---
+++ connpass-api-wrapper/handler.js
@@ -0,0 +1,31 @@
+'use strict';
+import axios from 'axios'
+
+// レスポンスのクラスを生成
+class Response {
+  constructor() {
+    this.statusCode = 200;
+    this.headers = {};
+    this.multiValueHeaders = {};
+    this.body = '';
+  }
+}
+module.exports.hello = async (event) => {
+  // レスポンスのインスタンスを生成
+  const response = new Response();
+  const params = event.queryStringParameters;
+  const userName = params.user_name
+
+  const res = await axios.get(`https://connpass.com/api/v1/event/?nickname=${userName}`)
+  .catch((e) => {
+    response.statusCode = e.response.status
+    response.body = e.response.data
+  })
+
+  response.body = JSON.stringify(res.data)
+
+  return response;
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
